336dad97c3a3bef85d5911076a9a3fe337472435
ryanchall <62118049+ryanchall@users.noreply.github.com>
2020-05-24 14:54:25 -0700

    Removing inline JS

--- archive_script.js
+++ archive_script.js
@@ -1,11 +1,10 @@
   /////////
  // WIP //
-/////////   
+/////////
 
-comments = document.getElementsByClassName("_1YCqQVO-9r-Up6QPB9H6_4 _1YCqQVO-9r-Up6QPB9H6_4")[0].children;
+comments = document.getElementsByClassName("_1YCqQVO-9r-Up6QPB9H6_4 _1YCqQVO-9r-Up6QPB9H6_4")[0].children
 
 function get_deleted_content(undelete_button) {
-    console.log('running get_deleted_content...')
     id = undelete_button.parentElement.parentElement.parentElement.parentElement.id;
     
     requestURL = "https://api.pushshift.io/reddit/search/comment/?ids="+id;
@@ -27,27 +26,20 @@ function main(comments) {
         var comment = comments[i].firstChild.firstChild.lastChild.lastChild.lastChild;
         try {
             if (comment.firstChild.innerHTML == "Comment deleted by user") {
-                console.log(comment.firstChild.innerHTML)
-                console.log(comment)
-                my_HTML = '&nbsp;&nbsp;&nbsp;<button id="get-deleted-content" class="_374Hkkigy4E4srsI2WktEd">ATTEMPT to Reveal Deleted Comment</button>'
+                //console.log(comment.firstChild.innerHTML)
+                //console.log(comment)
+                my_HTML = '&nbsp;&nbsp;&nbsp;<button onclick="get_deleted_content(this)" class="_374Hkkigy4E4srsI2WktEd">ATTEMPT to Reveal Deleted Comment</button>'
                 comment.innerHTML = comment.innerHTML + my_HTML
             } 
         } catch (e) {console.log('CATCH');}
-        console.log('EXTENSION SORTA WORKING!');
+		console.log('EXTENSION SORTA WORKING!');
     }
     return false
 }
 
-window.addEventListener('load', pageFullyLoaded, false);
+window.addEventListener("load", pageFullyLoaded, false);
 function pageFullyLoaded(e) {
     main(comments)
 }
 
-$(document).ready(function() {
-    $("#get-deleted-content").click(handler);
-});
-function handler() {
-    get_deleted_content(this)
-}
-
-// CURRENT ISSUE: "Uncaught ReferenceError: get_deleted_content is not defined at HTMLButtonElement.onclick ((index):1)"
+// CURRENT ISSUE: the button is added briefly before Reddit finishes loading and "deletes" my changes. RIP. No solution direction known, do more research.
