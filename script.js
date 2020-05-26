0240e3dffb2b9b4d2d5368121b6c28c4c72d9444
ryanchall <62118049+ryanchall@users.noreply.github.com>
2020-05-25 23:36:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,50 +4,58 @@
 
 comments = document.getElementsByClassName("_1YCqQVO-9r-Up6QPB9H6_4 _1YCqQVO-9r-Up6QPB9H6_4")[0].children;
 
-function get_deleted_content(undelete_button) {
-    console.log('running get_deleted_content...')
-    id = undelete_button.parentElement.parentElement.parentElement.parentElement.id;
-    
-    requestURL = "https://api.pushshift.io/reddit/search/comment/?ids="+id;
-        function reqListener () {
-          response = this.response;
-          console.log(response.data[0].body);
-          alert(response.data[0].body);
-        }
-        var req = new XMLHttpRequest();
-        req.addEventListener("load", reqListener);
-        req.responseType = 'json';
-        req.open("GET", requestURL);
-        req.send();
-        return false
-}
-
 function main(comments) {
+    return_var = false;
     for(i=0; i<comments.length; i++) {
         var comment = comments[i].firstChild.firstChild.lastChild.lastChild.lastChild;
         try {
-            if (comment.firstChild.innerHTML == "Comment deleted by user") {
-                console.log(comment.firstChild.innerHTML)
-                console.log(comment)
+            if (comment.firstChild.innerHTML == "Comment deleted by user" && !comment.innerHTML.includes('&nbsp;&nbsp;&nbsp;<button id="get-deleted-content" class="_374Hkkigy4E4srsI2WktEd">ATTEMPT to Reveal Deleted Comment</button>')) {
+                //console.log(comment.firstChild.innerHTML)
+                //console.log(comment)
                 my_HTML = '&nbsp;&nbsp;&nbsp;<button id="get-deleted-content" class="_374Hkkigy4E4srsI2WktEd">ATTEMPT to Reveal Deleted Comment</button>'
                 comment.innerHTML = comment.innerHTML + my_HTML
-            } 
+                return_var = true
+            }
         } catch (e) {console.log('CATCH');}
-        console.log('EXTENSION SORTA WORKING!');
+        //console.log('EXTENSION SORTA WORKING!');
     }
-    return false
+    return return_var;
 }
 
 window.addEventListener('load', pageFullyLoaded, false);
 function pageFullyLoaded(e) {
-    main(comments)
+    window.setInterval(function(){
+        main(comments);
+        document.getElementById('get-deleted-content').onclick = function() {
+            get_deleted_content(this);
+        }
+    }, 3000);
 }
 
-$(document).ready(function() {
-    $("#get-deleted-content").click(handler);
-});
-function handler() {
-    get_deleted_content(this)
+window.onload = function(){
+    try {
+        document.getElementById('get-deleted-content').onclick = function() {
+            get_deleted_content(this);
+        }
+    } catch (e) {}
+}
+
+function get_deleted_content(undelete_button) {
+    console.log('running get_deleted_content...');
+    clearInterval();
+    id = undelete_button.parentElement.parentElement.parentElement.parentElement.id;
+    requestURL = "https://api.pushshift.io/reddit/search/comment/?ids="+id;
+    function reqListener () {
+        response = this.response;
+        console.log(response.data[0].body);
+        alert(response.data[0].body);
+    }
+    var req = new XMLHttpRequest();
+    req.addEventListener("load", reqListener);
+    req.responseType = 'json';
+    req.open("GET", requestURL);
+    req.send();
+    return false
 }
 
 // CURRENT ISSUE: "Uncaught ReferenceError: get_deleted_content is not defined at HTMLButtonElement.onclick ((index):1)"
