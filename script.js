e6fb7d32e61423fd276d89e3e055e0fdda0786ac
ryanchall <62118049+ryanchall@users.noreply.github.com>
2020-05-26 12:03:21 -0700

    More debugging

--- script.js
+++ script.js
@@ -2,21 +2,26 @@
  // WIP //
 /////////   
 
-comments = document.getElementsByClassName("_1YCqQVO-9r-Up6QPB9H6_4 _1YCqQVO-9r-Up6QPB9H6_4")[0].children;
+try {
+    comments = document.getElementsByClassName("_1YCqQVO-9r-Up6QPB9H6_4 _1YCqQVO-9r-Up6QPB9H6_4")[0].children;
+} catch (e) {
+    comments = document.getElementsByClassName("_1YCqQVO-9r-Up6QPB9H6_4 _1YCqQVO-9r-Up6QPB9H6_4").children; //might be doing nothing at all...
+    alert('IMPORTANT CATCH');
+}
 
 function main(comments) {
     return_var = false;
     for(i=0; i<comments.length; i++) {
         var comment = comments[i].firstChild.firstChild.lastChild.lastChild.lastChild;
         try {
-            if (comment.firstChild.innerHTML == "Comment deleted by user" && !comment.innerHTML.includes('&nbsp;&nbsp;&nbsp;<button id="get-deleted-content" class="_374Hkkigy4E4srsI2WktEd">ATTEMPT to Reveal Deleted Comment</button>')) {
+            if (comment.firstChild.innerHTML == "Comment deleted by user" && !comment.innerHTML.includes('&nbsp;&nbsp;&nbsp;<button id="get-deleted-content" class="_374Hkkigy4E4srsI2WktEd">ATTEMPT to Reveal Deleted Comment</button>') && !comment.includes(' more repl')) {
                 //console.log(comment.firstChild.innerHTML)
                 //console.log(comment)
                 my_HTML = '&nbsp;&nbsp;&nbsp;<button id="get-deleted-content" class="_374Hkkigy4E4srsI2WktEd">ATTEMPT to Reveal Deleted Comment</button>'
                 comment.innerHTML = comment.innerHTML + my_HTML
                 return_var = true
             }
-        } catch (e) {console.log('CATCH');}
+        } catch (e) {console.log('CATCH'); console.log(comment);}
         //console.log('EXTENSION SORTA WORKING!');
     }
     return return_var;
@@ -26,9 +31,11 @@ window.addEventListener('load', pageFullyLoaded, false);
 function pageFullyLoaded(e) {
     window.setInterval(function(){
         main(comments);
-        document.getElementById('get-deleted-content').onclick = function() {
-            get_deleted_content(this);
-        }
+        try {
+            document.getElementById('get-deleted-content').onclick = function() {
+                get_deleted_content(this);
+            }
+        } catch (e) {}
     }, 3000);
 }
 
@@ -47,8 +54,13 @@ function get_deleted_content(undelete_button) {
     requestURL = "https://api.pushshift.io/reddit/search/comment/?ids="+id;
     function reqListener () {
         response = this.response;
-        console.log(response.data[0].body);
-        alert(response.data[0].body);
+        try {
+            console.log(response.data[0].body);
+            alert(response.data[0].body);
+        } catch (e) {
+            console.log('Comment was deleted too quickly (ALMOST 100% SURE)');
+            alert('Comment was deleted too quickly (ALMOST 100% SURE)');
+        }
     }
     var req = new XMLHttpRequest();
     req.addEventListener("load", reqListener);
@@ -58,4 +70,4 @@ function get_deleted_content(undelete_button) {
     return false
 }
 
-// CURRENT ISSUE: "Uncaught ReferenceError: get_deleted_content is not defined at HTMLButtonElement.onclick ((index):1)"
+// CURRENT ISSUE: type it out
