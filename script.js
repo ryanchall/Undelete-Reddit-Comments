96d1e15989aad74be085cdc433d031b43cfc5ed3
ryanchall <62118049+ryanchall@users.noreply.github.com>
2020-06-01 20:03:18 -0700

    Debugging, minimizing errors, added functionality
    A couple more logs, another try-catch, and allowing undeleting mod-removed comments.

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function main(comments) {
     for(i=0; i<comments.length; i++) {
         var comment = comments[i].firstChild.firstChild.lastChild.lastChild.lastChild;
         try {
-            if (comment.firstChild.innerHTML == "Comment deleted by user" && !comment.innerHTML.includes('&nbsp;&nbsp;&nbsp;<button id="get-deleted-content" class="_374Hkkigy4E4srsI2WktEd">ATTEMPT to Reveal Deleted Comment</button>')) {
+            if ((comment.firstChild.innerHTML == "Comment deleted by user" || comment.firstChild.innerHTML == "Comment removed by moderator") && !comment.innerHTML.includes('&nbsp;&nbsp;&nbsp;<button id="get-deleted-content" class="_374Hkkigy4E4srsI2WktEd">ATTEMPT to Reveal Deleted Comment</button>')) {
                 //console.log(comment.firstChild.innerHTML)
                 //console.log(comment)
                 var my_HTML = '&nbsp;&nbsp;&nbsp;<button id="get-deleted-content" class="_374Hkkigy4E4srsI2WktEd">ATTEMPT to Reveal Deleted Comment</button>'
@@ -32,25 +32,28 @@ window.addEventListener('load', pageFullyLoaded, false);
 function pageFullyLoaded(e) {
     window.setInterval(function(){
         console.log('addEventListener load RUNNING');
-        comments = document.getElementsByClassName("_1YCqQVO-9r-Up6QPB9H6_4 _1YCqQVO-9r-Up6QPB9H6_4")[0].children;
-        main(comments);
-        document.getElementById('get-deleted-content').onclick = function() {
-            get_deleted_content(this);
-        }
-    }, 3000);
+        try {
+            comments = document.getElementsByClassName("_1YCqQVO-9r-Up6QPB9H6_4 _1YCqQVO-9r-Up6QPB9H6_4")[0].children;
+            main(comments);
+            document.getElementById('get-deleted-content').onclick = function() {
+                get_deleted_content(this);
+            }
+        } catch (e) {}
+    }, 1000);
 }
 
 window.onload = function(){
     try {
         document.getElementById('get-deleted-content').onclick = function() {
             get_deleted_content(this);
+            console.log('WHAT DOES THIS DO??');
         }
     } catch (e) {console.log('DELETE THIS ENTIRE onload FUNCTION THINGY I think');}
 }
 
 function get_deleted_content(undelete_button) {
     console.log('running get_deleted_content...');
-    clearInterval();
+    //clearInterval();
     var id = undelete_button.parentElement.parentElement.parentElement.parentElement.id;
     requestURL = "https://api.pushshift.io/reddit/search/comment/?ids="+id;
     function reqListener () {
