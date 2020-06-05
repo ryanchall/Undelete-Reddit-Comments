7453c4c7e3d91ed64ce32e1f1617e513175ea50e
ryanchall <62118049+ryanchall@users.noreply.github.com>
2020-06-05 11:25:04 -0700

    Remove comments & add try catch
    Try Catch will catch a comment that is deleted too quickly, supplying an appropriate notification instead of no notificaiton at all.

--- script.js
+++ script.js
@@ -13,14 +13,11 @@ function main(comments) {
         var comment = comments[i].firstChild.firstChild.lastChild.lastChild.lastChild;
         try {
             if ((comment.firstChild.innerHTML == "Comment deleted by user" || comment.firstChild.innerHTML == "Comment removed by moderator") && !comment.innerHTML.includes('&nbsp;&nbsp;&nbsp;<button id="get-deleted-content" class="_374Hkkigy4E4srsI2WktEd">ATTEMPT to Reveal Deleted Comment</button>')) {
-                //console.log(comment.firstChild.innerHTML)
-                //console.log(comment)
                 var my_HTML = '&nbsp;&nbsp;&nbsp;<button id="get-deleted-content" class="_374Hkkigy4E4srsI2WktEd">ATTEMPT to Reveal Deleted Comment</button>'
                 comment.innerHTML = comment.innerHTML + my_HTML
                 return_var = true
             }
         } catch (e) {console.log('CATCH');}
-        //console.log('EXTENSION SORTA WORKING!');
     }
     if (return_var == false) {
         console.log('No comments to undelete??');
@@ -33,7 +30,6 @@ function main(comments) {
 window.addEventListener('load', pageFullyLoaded, false);
 function pageFullyLoaded(e) {
     window.setInterval(function(){
-        console.log('addEventListener load RUNNING');
         try {
             comments = document.getElementsByClassName("_1YCqQVO-9r-Up6QPB9H6_4 _1YCqQVO-9r-Up6QPB9H6_4")[0].children;
             main(comments);
@@ -47,21 +43,24 @@ function pageFullyLoaded(e) {
 /* window.onload = function(){} */
 
 function get_deleted_content(undelete_button) {
-    console.log('running get_deleted_content...');
-    //clearInterval();
+    //console.log('running get_deleted_content...');
     var id = undelete_button.parentElement.parentElement.parentElement.parentElement.id;
     requestURL = "https://api.pushshift.io/reddit/search/comment/?ids="+id;
     function reqListener () {
         response = this.response;
-        console.log(response.data[0].body);
-        alert(response.data[0].body);
+        try {
+            console.log('Deleted comment:\n\n' + response.data[0].body);
+            alert('Deleted comment:\n\n' + response.data[0].body);
+        } catch (e) {
+            console.log("DELETED TOO QUICKLY");
+            alert("DELETED TOO QUICKLY");
+        }
     }
     var req = new XMLHttpRequest();
     req.addEventListener("load", reqListener);
     req.responseType = 'json';
     req.open("GET", requestURL);
     req.send();
-    return false
 }
 
-// CURRENT ISSUE: updated 'comments' var is not correctly being updated and passed to main(comments)
+// CURRENT ISSUE: button does not always appear. Sometimes takes a page refresh to appear. Solution unknown atm.
